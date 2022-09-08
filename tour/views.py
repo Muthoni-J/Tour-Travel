856591fc5452e6f9f771c987c4b2b3206d908c05
Muthoni-J <jmjoanmuthoni@gmail.com>
2022-09-08 19:52:14 +0300

    changes

--- tour/views.py
+++ tour/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from tour.models import Tour
+from . import 
 
+
+def register_customers(request):
+    form = CustomerRegistrationForm()
+    return render(request,"wallet/register_customers.html",
+                  {"form":form})
 # Create your views here.
